2a9216df5b6359e2b790ef9e7ff295f8c43ab512
Reyhaneh <reyhaneh.sa.mo@gmail.com>
2025-02-07 07:17:07 +0330

    add evenFilter and jokeRandom Function

---
+++ app.js
@@ -0,0 +1,24 @@
+const arr=[12, 11, 0, 7, 54, 63, 81, 19, 5, 2];
+const evenFilter= (arr) => {
+    let arrFilter;
+    arrFilter= arr.filter(item => item%2===1)
+    console.log(arrFilter);
+}
+evenFilter(arr)
+
+let container= document.querySelector(".container");
+const jokeRandom= async () => {
+    let content=[];
+    // while(!content.length) {
+    //     container.textContent= "Loading"
+    // }
+    container.textContent="";
+    const URL= "https://api.chucknorris.io/jokes/random"
+    const response= await fetch(URL, {
+        method:"GET"
+    });
+    content= await response.json();
+    console.log(content)
+}
+jokeRandom();
+
